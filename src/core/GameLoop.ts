dff63f5ccd09425c8eb484618ce7b66cb3f0585d
Nelstar-the-Demonic <dnelstar872@gmail.com>
2026-05-19 18:47:05 +0200

    Create first Game Loop

--- src/core/GameLoop.ts
+++ src/core/GameLoop.ts
@@ -1,3 +1,31 @@
+import { GameState } from "./GameState"
+import { EventSystem } from "./systems/EventSystem"
+
 export class GameLoop {
+
+  static nextMonth() {
+
+    const player = GameState.player
+
+    player.age += 0.08
+
+    const event = EventSystem.getRandomEvent(player)
+
+    console.log("New Month")
+
+    console.log("Age:", player.age.toFixed(1))
+
+    if (event) {
+
+      console.log("Event:", event.title)
+      console.log(event.description)
+
+    } else {
+
+      console.log("No event this month.")
+
+    }
+
+  }
 
 }
