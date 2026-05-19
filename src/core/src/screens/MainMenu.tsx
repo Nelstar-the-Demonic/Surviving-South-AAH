45a44d474e62cf3d8f4a48487388e1321dca0db9
Nelstar-the-Demonic <dnelstar872@gmail.com>
2026-05-19 18:50:43 +0200

    Create main menu screen

--- src/core/src/screens/MainMenu.tsx
+++ src/core/src/screens/MainMenu.tsx
@@ -1,3 +1,28 @@
-export MainMenu {
+import React from "react"
+import { View, Text, Button } from "react-native"
+
+export default function MainMenu({ navigation }: any) {
+
+  return (
+
+    <View style={{ padding: 20 }}>
+
+      <Text
+        style={{
+          fontSize: 32,
+          marginBottom: 20
+        }}
+      >
+        Surviving South A
+      </Text>
+
+      <Button
+        title="New Game"
+        onPress={() => navigation.navigate("Game")}
+      />
+
+    </View>
+
+  )
 
 }
