f423f5252371df93aa5ae3ef9e3de36302d6a007
Nelstar-the-Demonic <dnelstar872@gmail.com>
2026-05-19 18:39:28 +0200

    Create basic stat system

--- src/core/src/core/src/systems/src/systems/StatSystem.ts
+++ src/core/src/core/src/systems/src/systems/StatSystem.ts
@@ -1,3 +1,19 @@
-export StatSystem {
+export class StatSystem {
+
+  static updateStat(player: any, stat: string, amount: number) {
+
+    if (player.stats[stat] !== undefined) {
+      player.stats[stat] += amount
+    }
+
+    if (player.battleStats[stat] !== undefined) {
+      player.battleStats[stat] += amount
+    }
+
+    if (player.workStats[stat] !== undefined) {
+      player.workStats[stat] += amount
+    }
+
+  }
 
 }
