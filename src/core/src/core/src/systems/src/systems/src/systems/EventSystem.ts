7f0a2714c315b3ce8b3ac82c391c4848e1d5dab9
Nelstar-the-Demonic <dnelstar872@gmail.com>
2026-05-19 18:43:00 +0200

    Create basic events systemm

--- src/core/src/core/src/systems/src/systems/src/systems/EventSystem.ts
+++ src/core/src/core/src/systems/src/systems/src/systems/EventSystem.ts
@@ -1,3 +1,26 @@
-export EventSystem {
+import events from "../../data/events.json"
+
+export class EventSystem {
+
+  static getRandomEvent(player: any) {
+
+    const validEvents = events.filter((event: any) => {
+
+      return (
+        player.age >= event.minAge &&
+        player.age <= event.maxAge &&
+        event.locations.includes(player.location)
+)
+    })
+
+    if (validEvents.length === 0) {
+      return null
+    }
+
+    const randomIndex = Math.floor(Math.random() * validEvents.length)
+
+    return validEvents[randomIndex]
+
+  }
 
 }
