180bee4513451274c190784f589506b08d16f879
Deamon Nelstar <dnelstar872@gmail.com>
2026-06-18 15:20:07 +0200

    Remove Miaoda Metro config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withDevkit } = require('miaoda-expo-devkit/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withDevkit(config);
+module.exports = config;
