29cc1e73cdc6eb3bc6e865eb33d6805d3013227f
Deamon Nelstar <dnelstar872@gmail.com>
2026-06-18 18:43:56 +0200

    Remove Miaoda Babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  presets: [['miaoda-expo-devkit/babel-preset', { excludePaths: ['src/components/ui'] }]],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
